83aa763ff6ba8d2b3ff2d4a9a7e3f92c7fcdbef3
Grach <Pavlov-Maxim.S@yandex.ru>
2020-11-27 19:25:42 +0300

    Revert "Revert "Тест""
    This reverts commit 9c39be9626b9d7e9eadb44432336f128d6897dca.

--- Program.cs
+++ Program.cs
@@ -25,6 +25,9 @@ namespace Scout
 
             Console.WriteLine("'Разведчик' закончил работу || The 'Scout' finished his work: ...");
             //Console.ReadLine();
+
+
+            //Тест
         }
     }
 }
